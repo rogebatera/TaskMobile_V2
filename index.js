0ec60a6d269defef8d35d75d5dec51ed8bc59551
Rogerio Lucio <rogeriob.lucio@hotmail.com>
2020-03-10 23:12:37 -0300

    Primeiro Commit Geral

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Task from './src/screens/TaskList';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Task);
